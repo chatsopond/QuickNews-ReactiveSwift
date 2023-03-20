3a05b24664f4b903e2bae9e66686db44911aaa56
Chatsopon Deepateep <chatsopon_acc@icloud.com>
2023-03-20 13:39:40 +0700

    Initial Commit

---
+++ QuickNews-ReactiveSwift/QuickNews_ReactiveSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNews_ReactiveSwiftApp.swift
+//  QuickNews-ReactiveSwift
+//
+//  Created by Chatsopon Deepateep on 20/3/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickNews_ReactiveSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
