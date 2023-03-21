64d8b7abc117419bfc8c2961d9b7fd9a6c25543c
Chatsopon Deepateep <chatsopon_acc@icloud.com>
2023-03-21 14:31:16 +0700

    Update comments for better code documentation and clarity.

--- API/SampleAPI.swift
+++ API/SampleAPI.swift
@@ -10,17 +10,23 @@ import Moya
 import ReactiveSwift
 import ReactiveMoya
 
+/// SampleAPI enum represents the different API endpoints available.
 enum SampleAPI {
+  /// Endpoint for getting the current time.
   case getTime
+  /// Endpoint for getting a random integer between the specified lower and upper bounds.
   case getRandomInteger(lowerBound: Int, upperBound: Int)
 }
 
+// Extend SampleAPI to conform to Moya's TargetType protocol
 extension SampleAPI: TargetType {
+  /// The base URL for the API requests.
   var baseURL: URL {
     // swiftlint:disable:next force_unwrapping
     return URL(string: "https://iced-bejewled-ranunculus.glitch.me")!
   }
 
+  /// The path for each case of the API.
   var path: String {
     switch self {
     case .getTime:
@@ -31,6 +37,7 @@ extension SampleAPI: TargetType {
     }
   }
 
+  /// The HTTP method for each case of the API.
   var method: Moya.Method {
     switch self {
       // swiftlint:disable:next empty_enum_arguments
@@ -39,6 +46,7 @@ extension SampleAPI: TargetType {
     }
   }
 
+  /// The task for each case of the API.
   var task: Task {
     switch self {
     case .getTime:
@@ -49,6 +57,7 @@ extension SampleAPI: TargetType {
     }
   }
 
+  /// The headers for the API requests.
   var headers: [String: String]? {
     return ["Content-Type": "application/json"]
   }
@@ -56,11 +65,15 @@ extension SampleAPI: TargetType {
 
 // MARK: - ReactiveSwift + Moya
 
+/// A custom error type for SampleAPI.
 enum SampleAPIError: Error {
   case noData
 }
 
+/// A struct representing the API response, conforming to Decodable, Identifiable, and Hashable.
 struct SampleAPIResponse: Decodable, Identifiable, Hashable {
+  /// A unique identifier for each response object.
   var id = UUID()
+  /// The result string from the API response.
   var result: String?
 }
--- View Model/APIListViewModel.swift
+++ View Model/APIListViewModel.swift
@@ -10,15 +10,16 @@ import Moya
 import ReactiveMoya
 import ReactiveSwift
 
+/// APIListViewModel is a class that manages the data and API requests for the SampleAPI.
 class APIListViewModel: ObservableObject {
   /// Published property to hold `SampleAPIResponse` and notify UI of changes
   @Published var responses: [SampleAPIResponse] = []
 
   let sampleAPIProvider = MoyaProvider<SampleAPI>()
 
-  /// Signal for observing the coming sample api response
+  /// Signal for observing the coming `SampleAPIResponse`
   var sampleAPIResponseSignal: Signal<SampleAPIResponse, Error>?
-  /// Observer for sending the new array of articles
+  /// Observer for sending the new `SampleAPIResponse`
   var sampleAPIResponseObserver: Signal<SampleAPIResponse, Error>.Observer?
 
   /// CompositeDisposable to manage the lifecycle of the subscriptions
@@ -29,6 +30,7 @@ class APIListViewModel: ObservableObject {
     (sampleAPIResponseSignal, sampleAPIResponseObserver) =
     Signal<SampleAPIResponse, Error>.pipe(disposable: compositeDisposable)
 
+    // Observe the SampleAPI response signal
     sampleAPIResponseSignal?
       .observeResult { [weak self] result in
         guard let self else { return }
@@ -49,6 +51,7 @@ class APIListViewModel: ObservableObject {
 // MARK: - APIs
 
 extension APIListViewModel {
+  /// Requests the current time from the SampleAPI and updates the responses array.
   func getNow() {
     sampleAPIProvider
       .reactive
@@ -70,7 +73,8 @@ extension APIListViewModel {
         }
       }
   }
-
+  
+  /// Requests a random integer between the specified lower and upper bounds from the SampleAPI and updates the responses array.
   func getRandomInteger(_ lowerbound: Int, _ upperbound: Int) {
     sampleAPIProvider
       .reactive
